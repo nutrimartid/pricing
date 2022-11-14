5d6881123b67ce9eda353085d74c86f3409cc0e8
nutrimartid <nutrimartid@gmail.com>
2022-11-14 11:17:38 +0700

    style form

--- app.py
+++ app.py
@@ -21,21 +21,29 @@ class tbluser(db.Model):
 
 @app.route('/',methods=['POST','GET'])
 def index():
-    df=tbluser.query.all()
-    print(df)
-    if request.method=='POST':
-        newuser=tbluser(first_name=request.form['fname'],last_name=request.form['lname'])
-        db.session.add(newuser)
-        db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('home.html',df=df)
+    # df=tbluser.query.all()
+    # print(df)
+    # if request.method=='POST':
+    #     newuser=tbluser(first_name=request.form['fname'],last_name=request.form['lname'])
+    #     db.session.add(newuser)
+    #     db.session.commit()
+    #     return redirect(url_for('index'))
+    return render_template('home2.html')
 
-@app.route('/delete<id>',methods=['POST','GET'])
-def delete(id):
-    deluser=tbluser.query.get(id)
-    db.session.delete(deluser)
-    db.session.commit()
-    return redirect(url_for('index'))
+# @app.route('/delete<id>',methods=['POST','GET'])
+# def delete(id):
+#     deluser=tbluser.query.get(id)
+#     db.session.delete(deluser)
+#     db.session.commit()
+#     return redirect(url_for('index'))
+
+@app.route('/form',methods=['POST','GET'])
+def form():
+    return render_template('form.html')
+
+@app.route('/form2',methods=['POST','GET'])
+def form2():
+    return render_template('form2.html')
 
 if __name__ == '__main__':
     app.run()
