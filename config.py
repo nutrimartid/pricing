e155af7e70eb888d444c7d3696435b46b96b41fd
nutrimartid <nutrimartid@gmail.com>
2022-11-21 11:25:02 +0700

    add sec key

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI= 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY='Lokalat3'
