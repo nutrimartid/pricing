2f95d219eec70855377798f74c013a797f5ff510
nutrimartid <nutrimartid@gmail.com>
2022-11-21 11:34:04 +0700

    mod config

--- config.py
+++ config.py
@@ -4,8 +4,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI= 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-<<<<<<< HEAD
 SECRET_KEY='lokalat3'
-=======
-SECRET_KEY='lokalat3'
->>>>>>> 4202876 ('done)
