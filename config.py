2de2d18da66fc4b95bb893083ca7ff8b248b12cd
nutrimartid <nutrimartid@gmail.com>
2022-12-05 16:38:50 +0700

    add imagekit storage

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ SQLALCHEMY_DATABASE_URI= 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY='lokalat3'
 UPLOAD_FOLDER='static/upload'
+PRIKEYIMGKIT="private_EJ1X2wEqxTbf647Lf9KEdUayw5E="
+PUBKEYIMGKIT='public_GZm9njJXmrcaOR96i2v821Rx3Fg='
+URLENDIMGKIT='https://ik.imagekit.io/z83ycl28q'
